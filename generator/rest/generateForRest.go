fa663bf8ee592bf9b4ff3a070eeb0cabc4f8922e
Marcus Martina <marcus@marcuss-mbp.ert.lan>
2017-02-13 14:42:22 +0100

    renamed to enduserAccess

--- generator/rest/generateForRest.go
+++ generator/rest/generateForRest.go
@@ -495,7 +495,7 @@ func {{$oper.Name}}( service *{{$structName}} ) http.HandlerFunc {
 				"language": language,
 				"requestUid": r.Header.Get("X-request-uid"),
 				"sessionUid":  r.Header.Get("X-session-uid"),
-				"enduserPermission": r.Header.Get("X-enduser-permission"),
+				"enduserAccess": r.Header.Get("X-enduser-access"),
 				"enduserRole": r.Header.Get("X-enduser-role"),
 				"enduserUid": r.Header.Get("X-enduser-uid"),
 			}
