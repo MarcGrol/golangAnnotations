45002b159b083c6694fd57aa1cf41a145ff6459f
Marc Grol <mgrol@xebia.com>
2017-06-01 15:34:20 +0200

    Add extraction of basic auth header from service-request

--- generator/rest/restSupport.go
+++ generator/rest/restSupport.go
@@ -1,8 +1,25 @@
 package rest
 
-import "net/http"
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func ExtractCredentials(language string, r *http.Request) Credentials {
+	username, password, err := decodeBasicAuthHeader(r)
+	if err == nil {
+		return Credentials{
+			Language:      language,
+			RequestUID:    r.Header.Get("X-request-uid"),
+			SessionUID:    "",
+			EndUserAccess: "",
+			EndUserRole:   "supplier",
+			EndUserUID:    username,
+			Password:      password,
+		}
+	}
 	return Credentials{
 		Language:      language,
 		RequestUID:    r.Header.Get("X-request-uid"),
@@ -20,4 +37,21 @@ type Credentials struct {
 	EndUserAccess string
 	EndUserRole   string
 	EndUserUID    string
+	Password      string
+}
+
+func decodeBasicAuthHeader(r *http.Request) (string, string, error) {
+	auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
+	if len(auth) != 2 || auth[0] != "Basic" {
+		return "", "", fmt.Errorf("Invalid/missing header")
+	}
+
+	payload, _ := base64.StdEncoding.DecodeString(auth[1])
+	pair := strings.SplitN(string(payload), ":", 2)
+	if len(pair) != 2 {
+		return "", "", fmt.Errorf("Invalid/missing header-values")
+	}
+
+	return pair[0], pair[1], nil
+
 }
